b49a3a44184803a840677355cf4980a6bf05d215
Larry Kann <kanndide@gmail.com>
2024-03-25 09:58:57 -0400

    updated lsp stuff

--- lua/atariaf/packer.lua
+++ lua/atariaf/packer.lua
@@ -13,7 +13,7 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    use({ 
+    use({
         "ellisonleao/gruvbox.nvim", 
         as = 'gruvbox',
         config = function()
